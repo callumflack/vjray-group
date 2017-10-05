7854ab29378f213198ba9d986e22da92e3b4fc19
Barry Hood <BarryPH@protonmail.com>
2017-10-06 11:30:53 +1300

    Reapply - Fix error "Trying to replace an element that wasn't mounted!"

--- pages/_document.js
+++ pages/_document.js
@@ -7,12 +7,13 @@ export default class MyDocument extends Document {
   render () {
     const sheet = new ServerStyleSheet()
     const main = sheet.collectStyles(<Main />)
-    const styleTags = sheet.getStyleElement()
+    const styleTags = sheet.getStyleTags()
+
     return (
       <html>
         <Head>
           <title>My page</title>
-          {styleTags}
+          <script dangerouslySetInnerHTML={{ __html: styleTags }} />
 
           <script src='/static/js/google-calls.js'></script>
         </Head>
