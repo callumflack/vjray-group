52084664c070118c14f9b21bb7836d87baceea58
Barry Hood <BarryPH@protonmail.com>
2017-10-16 16:27:32 +1300

    Add google tag manager script

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,11 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <script src='/static/js/google-tag-manager.js'></script>
+          <script src='/static/js/google-calls.js'></script>
+
           <title>My page</title>
           <script dangerouslySetInnerHTML={{ __html: styleTags }} />
-          <script src='/static/js/google-calls.js'></script>
         </Head>
         <body>
           <Provider theme={theme} className='root'>
