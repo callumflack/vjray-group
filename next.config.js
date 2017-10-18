134c188ebd14731f24e5c8dda91b1aafe7df089e
Callum Flack <callumflack@gmail.com>
2017-10-18 14:32:02 +1000

    basic setups for GTM & formspree

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = {
       '/': { page: '/' },
       '/strata-management': { page: '/strata-management' },
       '/rental-property-management': { page: '/rental-property-management' },
+      '/forms-and-fact-sheets': { page: '/forms-and-fact-sheets' },
       '/contact': { page: '/contact' },
       '/privacy-policy': { page: '/privacy-policy' },
       '/terms-of-use': { page: '/terms-of-use' }
