5a34117c5f87f78653e7f24963085206ba3a2501
Callum Flack <callumflack@gmail.com>
2017-10-20 21:13:29 +1000

    experimenting w/ ESlint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
     "extends": "airbnb",
     "rules": {
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "quotes": ["error", "double"],
+        "semi": ["error", "never"]
     }
 };
