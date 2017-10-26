d03975a5ffcd9bea5a7289b242e19ee043f98e70
Callum Flack <callumflack@gmail.com>
2017-10-26 17:53:49 +1000

    eslint reformats it's own config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-    "extends": "airbnb",
-    "rules": {
-        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-        "quotes": ["error", "double"],
-        "semi": ["error", "never"]
-    }
-};
+  extends: "airbnb",
+  rules: {
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    quotes: ["error", "double"],
+    semi: ["error", "never"],
+  },
+}
