7fc31af6b24a210811bdb11cd37aef271ee65fa4
Callum Flack <callumflack@gmail.com>
2017-10-27 19:14:03 +1000

    eslint-config-airbnb setup w/ prettier dependency
    load prettier via your editor, no npm installation, and use the
    prettier defaults

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,18 @@
 module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  // Save time, use a shareable config with the extends feature of .eslintrc files.
+  // Configs to consider:
+  // https://github.com/yannickcr/eslint-plugin-react#recommended
+  // Extend with airbnb's eslint config (which uses eslint-plugin-react):
+  // https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb/rules
   extends: "airbnb",
   rules: {
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-    quotes: ["error", "double"],
-    semi: ["error", "never"],
-  },
-}
+    "quotes": ["error", "double"],
+    // overwrite https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
+    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }]
+  }
+};
