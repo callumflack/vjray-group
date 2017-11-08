adc4c3eac91f912214b83dabccc6c50a6c961565
Callum Flack <callumflack@gmail.com>
2017-11-08 14:56:34 +1000

    add valuations copy

--- components/constants.js
+++ components/constants.js
@@ -1,6 +1,6 @@
 // site type flag
 
-const site = "group"
+const site = "group";
 
 // Nav items with the label 'Home' will not be shown
 // in the desktop nav
@@ -26,7 +26,7 @@ const navItems = [
     label: "Contact",
     href: "/contact",
   },
-]
+];
 
 // Location lists
 
@@ -61,7 +61,7 @@ const locationList = [
     googleMapsUrl: "https://www.google.com.au/maps/place/V.J.+Ray/@-34.0432583,151.1198929,17z",
     imageName: "caringbah.jpg",
   },
-]
+];
 
 // site contacts
 
@@ -69,7 +69,7 @@ const contactDetails = {
   phone: "1300 073 405",
   emergencyAfterHours: "0418 245 545",
   emergencyBusinessHours: "9784 7900",
-}
+};
 
 // Used in the contacts.js component
 // Each page will show the locations in listed in
@@ -91,7 +91,7 @@ const contactLocationsForPathnames = {
   "/contact": {
     locations: ["Campsie Head Office", "Caringbah Office"],
   },
-}
+};
 
 // "Home pg features" content
 
@@ -117,7 +117,7 @@ const featuresList = [
     lede: "Trust the price and act with confidence. Qualified building valuations done right.",
     link: "/property-valuations",
   },
-]
+];
 
 // "emergency" content
 
@@ -147,12 +147,12 @@ const emergencyList = [
     text:
     "For non-urgent repairs please use the form below and provide us as much detail as possible about your repair request.",
   },
-]
+];
 
 // "services" content
 
 const servicesIntro =
-  "We do everything you’d expect: arrange meetings, collect levies, manage repairs and help with disputes. But we believe it shouldn’t stop there. Here’s how we go further:"
+  "We do everything you’d expect: arrange meetings, collect levies, manage repairs and help with disputes. But we believe it shouldn’t stop there. Here’s how we go further:";
 
 const servicesList = [
   {
@@ -206,12 +206,12 @@ const servicesList = [
     "We are constantly working to improve our help to strata owners and residents by taking advantage of technology as it becomes available. Things such as video meetings, an App to report repairs and maintenance items, and online contractor supervision are just a few of the things we are currently working on, so be sure to check back soon to see how they are coming along.",
     link: false,
   },
-]
+];
 
 // "Property management" content
 
 const landlordIntro =
-  "With over 55 years experience of managing rental properties for landlords we know what's needed to help you get the best from their investment with the least amount of fuss. Here's 3 of the many ways we'll sort it for you."
+  "With over 55 years experience of managing rental properties for landlords we know what's needed to help you get the best from their investment with the least amount of fuss. Here's 3 of the many ways we'll sort it for you.";
 
 const landlordList = [
   {
@@ -235,10 +235,10 @@ const landlordList = [
     "We recognise that our landlords might not be available when we are open, so you can visit our online landlords portal  to get the latest information about your property.",
     link: false,
   },
-]
+];
 
 const tenantIntro =
-  "When you become a VJ Ray tenant, we're here to help you too. Here are just a couple of ways."
+  "When you become a VJ Ray tenant, we're here to help you too. Here are just a couple of ways.";
 
 const tenantList = [
   {
@@ -262,12 +262,12 @@ const tenantList = [
     "24 hours a day, 7 days a week you can access your details on line through our tenant portal. to check where your rent is paid to or update your contact details.",
     link: false,
   },
-]
+];
 
 // "Strata" content
 
 const strataIntro =
-  "Intro paragraph selling our Strata Management services going over two or three lines."
+  "Intro paragraph selling our Strata Management services going over two or three lines.";
 
 const strataList = [
   {
@@ -291,7 +291,7 @@ const strataList = [
     "We know that not everyone has time during the week, so unlike most Stata management companies we also open on Saturdays, so you can pop in, call us or even have your meeting on a Saturday!",
     link: false,
   },
-]
+];
 
 // "Valuations" content
 
@@ -300,39 +300,51 @@ const valuationsList = [
     number: "1",
     headline: "Strata Building Insurance Valuations",
     text:
-    "We know that emergencies don’t just happen during office hours. And rather than just leave you with the phone number for a message service we have our most senior staff on call to help you with emergency repairs, whenever they occur.",
+    "We carry out in excess of 1500 insurance valuations per year and can quickly turn around your request with the full knowledge of the requirements of the Strata Schemes Management Act.",
   },
   {
     number: "2",
-    headline: "Capital gains tax",
+    headline: "Stamp Duty Valuations",
     text:
-    "We all know how complex the licensing, WHS & insurance laws surrounding contractors are these days, so at VJ Ray we enlist the help of an independent company to continually monitor each and every contractor used by all of our strata schemes so that there is never any doubt that they comply.",
+    "We carry out valuations that meet the requirements of the office of State Revenue to assist you in meeting your stamp duty obligations.",
   },
   {
     number: "3",
-    headline: "Litigation and family law",
+    headline: "Capital Gains Tax Valuations",
     text:
-    "We know that not everyone has time during the week, so unlike most Stata management companies we also open on Saturdays, so you can pop in, call us or even have your meeting on a Saturday!",
+    "Whether current or retrospective reports we have the expertise and knowledge to carry out valuations which will help you when assessing your capital gains tax liability.",
   },
   {
     number: "4",
-    headline: "Pre-sale and pre-purchase advice",
+    headline: "Fair Market Value Valuations",
     text:
-    "We know that not everyone has time during the week, so unlike most Stata management companies we also open on Saturdays, so you can pop in, call us or even have your meeting on a Saturday!",
+    "Should you need to ascertain the present or retrospective value of a property we can help you by providing a valuation report with the minimum of fuss.",
   },
   {
     number: "5",
-    headline: "Market value for stamp duty",
+    headline: "Self Funded Superannuation",
     text:
-    "We know that not everyone has time during the week, so unlike most Stata management companies we also open on Saturdays, so you can pop in, call us or even have your meeting on a Saturday!",
+    "We are able to supply valuations for superannuation purposes on any property at a set date and on a ongoing basis.",
   },
   {
     number: "6",
-    headline: "Self-managed super funds",
+    headline: "Pre-Sale and Pre-Purchase",
     text:
-    "We know that not everyone has time during the week, so unlike most Stata management companies we also open on Saturdays, so you can pop in, call us or even have your meeting on a Saturday!",
+    "With over 50 years experience in real estate we have the knowledge and expertise to advise on the pre purchase or the pre-sale values, when you need advice that is independent of the selling agent.",
   },
-]
+  {
+    number: "7",
+    headline: "Family Law Valuations",
+    text:
+    "With extensive experience in the Family Law we are in a position to issue valuations for all stages of Family Law proceedings.",
+  },
+  {
+    number: "8",
+    headline: "Foreign Ownership Valuations and Advice",
+    text:
+    "With the ever changing area of foreign ownership regulation, we are able to provide you with valuation reports to assist you in your dealing with all government departments.",
+  },
+];
 
 // Facts and forms
 
@@ -342,7 +354,7 @@ const forms = [
     subheadline: "Form example subheadline",
     link: "/static/forms/example.pdf",
   },
-]
+];
 
 const factSheets = [
   {
@@ -350,7 +362,7 @@ const factSheets = [
     subheadline: "Fact Sheet example subheadline",
     link: "/static/fact-sheets/example.pdf",
   },
-]
+];
 
 export {
   site,
@@ -370,4 +382,4 @@ export {
   valuationsList,
   forms,
   factSheets,
-}
+};
