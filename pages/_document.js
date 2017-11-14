981ad726d5c0e824ac23cf25ed655cbf77ccebc6
Callum Flack <callumflack@gmail.com>
2017-11-14 14:35:27 +1000

    prettier

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,15 @@
-import React from "react"
-import Document, { Head, Main, NextScript } from "next/document"
-import { ServerStyleSheet } from "styled-components"
+import React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   render() {
-    const sheet = new ServerStyleSheet()
-    const main = sheet.collectStyles(<Main />)
-    const styleTags = sheet.getStyleTags()
+    const sheet = new ServerStyleSheet();
+    const main = sheet.collectStyles(<Main />);
+    const styleTags = sheet.getStyleTags();
 
     return (
-      <html>
+      <html lang="en">
         <Head>
           <script>dataLayer = [];</script>
           <script src="/static/js/google-tag-manager.js" />
@@ -19,13 +19,14 @@ export default class MyDocument extends Document {
         <body>
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TNFDHLZ" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`
+              __html:
+                "<iframe src=\"https://www.googletagmanager.com/ns.html?id=GTM-TNFDHLZ\" height=\"0\" width=\"0\" style=\"display:none;visibility:hidden;\"></iframe>",
             }}
           />
           <div className="root">{main}</div>
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
