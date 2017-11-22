37fcc69d175a3159a4c221541030948ff0e48664
Callum Flack <callumflack@gmail.com>
2017-11-22 18:06:58 +1000

    [lint]

--- next.config.js
+++ next.config.js
@@ -2,15 +2,14 @@
 // https://github.com/zeit/next.js#static-html-export
 
 module.exports = {
-  exportPathMap: () => {
-    return {
-      '/': { page: '/' },
-      '/strata-management': { page: '/strata-management' },
-      '/rental-property-management': { page: '/rental-property-management' },
-      '/forms-and-fact-sheets': { page: '/forms-and-fact-sheets' },
-      '/contact': { page: '/contact' },
-      '/privacy-policy': { page: '/privacy-policy' },
-      '/terms-of-use': { page: '/terms-of-use' }
-    }
-  }
-}
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/strata-management": { page: "/strata-management" },
+    "/rental-property-management": { page: "/rental-property-management" },
+    "/property-valuations": { page: "/property-valuations" },
+    "/forms-and-fact-sheets": { page: "/forms-and-fact-sheets" },
+    "/contact": { page: "/contact" },
+    "/privacy-policy": { page: "/privacy-policy" },
+    "/terms-of-use": { page: "/terms-of-use" },
+  }),
+};
