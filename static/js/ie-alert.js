38a2c9c495355aad681d20a2f00e38b5f905ae3e
Callum Flack <callumflack@gmail.com>
2018-04-19 18:10:49 +1000

    fix: use var for old IE

--- static/js/ie-alert.js
+++ static/js/ie-alert.js
@@ -1,4 +1,4 @@
-const ua = window.navigator.userAgent;
+var ua = window.navigator.userAgent;
 if (ua.indexOf("Trident") !== -1) {
   alert(
     "The VJ Ray website doesn't work well on Microsoft Internet Explorer. Please use a modern browser like Chrome, Edge or Safari instead."
