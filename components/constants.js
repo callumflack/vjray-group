288cb2410f9dee94a835966a062f2413ab5602ed
Callum Flack <callumflack@gmail.com>
2018-10-16 12:50:17 +1000

    Update components/constants.js

--- components/constants.js
+++ components/constants.js
@@ -12,24 +12,24 @@ const formspreeId = "mnpwvglm";
 const navItems = [
   {
     label: "Home",
-    href: "/",
+    href: "/"
   },
   {
     label: "Strata Management",
-    href: "/strata-management",
+    href: "/strata-management"
   },
   {
     label: "Property Management",
-    href: "/rental-property-management",
+    href: "/rental-property-management"
   },
   {
     label: "Valuations",
-    href: "/property-valuations",
+    href: "/property-valuations"
   },
   {
     label: "Contact",
-    href: "/contact",
-  },
+    href: "/contact"
+  }
 ];
 
 // Location lists
@@ -47,9 +47,8 @@ const locationList = [
     email: "campsie@vjray.com.au",
     poBox: "PO Box 369 Campsie, NSW 2194",
     emergencyPhoneNumber: "1300 073 405",
-    googleMapsUrl:
-      "https://www.google.com.au/maps/place/V+J+Ray/@-33.9113278,151.1011612,17z",
-    imageName: "campsie.jpg",
+    googleMapsUrl: "https://www.google.com.au/maps/place/V+J+Ray/@-33.9113278,151.1011612,17z",
+    imageName: "campsie.jpg"
   },
   {
     name: "Caringbah Office",
@@ -63,10 +62,9 @@ const locationList = [
     email: "caringbah@vjray.com.au",
     poBox: "PO Box 369 Campsie, NSW 2194",
     emergencyPhoneNumber: "1300 073 405",
-    googleMapsUrl:
-      "https://www.google.com.au/maps/place/V.J.+Ray/@-34.0432583,151.1198929,17z",
-    imageName: "caringbah.jpg",
-  },
+    googleMapsUrl: "https://www.google.com.au/maps/place/V.J.+Ray/@-34.0432583,151.1198929,17z",
+    imageName: "caringbah.jpg"
+  }
 ];
 
 // site contacts
@@ -74,7 +72,7 @@ const locationList = [
 const contactDetails = {
   phone: "1300 073 405",
   emergencyAfterHours: "0418 245 545",
-  emergencyBusinessHours: "9784 7900",
+  emergencyBusinessHours: "9784 7900"
 };
 
 // Used in the contacts.js component
@@ -83,20 +81,20 @@ const contactDetails = {
 
 const contactLocationsForPathnames = {
   "/": {
-    locations: ["Campsie Head Office", "Caringbah Office"],
+    locations: ["Campsie Head Office", "Caringbah Office"]
   },
   "/strata-management": {
-    locations: ["Campsie Head Office", "Caringbah Office"],
+    locations: ["Campsie Head Office", "Caringbah Office"]
   },
   "/rental-property-management": {
-    locations: ["Campsie Head Office"],
+    locations: ["Campsie Head Office"]
   },
   "/forms-and-fact-sheets": {
-    locations: ["Campsie Head Office", "Caringbah Office"],
+    locations: ["Campsie Head Office", "Caringbah Office"]
   },
   "/contact": {
-    locations: ["Campsie Head Office", "Caringbah Office"],
-  },
+    locations: ["Campsie Head Office", "Caringbah Office"]
+  }
 };
 
 // "Home pg features" content
@@ -106,25 +104,23 @@ const featuresList = [
     icon: "strata",
     border: true,
     headline: "Strata Management",
-    lede:
-      "Skip the hassle of Strata Management and get back your peace of mind.",
-    link: "/strata-management",
+    lede: "Skip the hassle of Strata Management and get back your peace of mind.",
+    link: "/strata-management"
   },
   {
     icon: "contents",
     border: true,
     headline: "Property Management",
     lede: "Get the best from your investment with the least amount of fuss.",
-    link: "/rental-property-management",
+    link: "/rental-property-management"
   },
   {
     icon: "valuations",
     border: true,
     headline: "Real Estate Valuations",
-    lede:
-      "Trust the price and act with confidence. Qualified building valuations done right.",
-    link: "/property-valuations",
-  },
+    lede: "Trust the price and act with confidence. Qualified building valuations done right.",
+    link: "/property-valuations"
+  }
 ];
 
 // "emergency" content
@@ -133,28 +129,28 @@ const emergencyList = [
   {
     headline: "Is it due to a natural disaster?",
     text:
-      "In the case of a natural disaster such as a major storm or flood etc please contact the NSW State Emergency Service on 132 500.",
+      "In the case of a natural disaster such as a major storm or flood etc please contact the NSW State Emergency Service on 132 500."
   },
   {
     headline: "Is it a rental property?",
     text:
-      "If your repair relates to a property that you are renting through one of our offices please go to the VJ Ray Group website.",
+      "If your repair relates to a property that you are renting through one of our offices please go to the VJ Ray Group website."
   },
   {
     headline: "Is it within business hours?",
     text:
-      "Please contact us by telephone on 9784 7900 if your repair is urgent and during office hours. Please don't rely on an email for an urgent repair as your strata manager may not see the email in time to contact the relevant repairer.",
+      "Please contact us by telephone on 9784 7900 if your repair is urgent and during office hours. Please don't rely on an email for an urgent repair as your strata manager may not see the email in time to contact the relevant repairer."
   },
   {
     headline: "Is it after hours?",
     text:
-      "For after hours emergency repairs only contact our director Mike Pollard on 0418 245 545. Note that additional charges will apply for after hour repairs.",
+      "For after hours emergency repairs only contact our director Mike Pollard on 0418 245 545. Note that additional charges will apply for after hour repairs."
   },
   {
     headline: "Is it really urgent?",
     text:
-      "For non-urgent repairs please use the form below and provide us as much detail as possible about your repair request.",
-  },
+      "For non-urgent repairs please use the form below and provide us as much detail as possible about your repair request."
+  }
 ];
 
 // "services" content
@@ -170,50 +166,50 @@ const servicesList = [
       "We know that emergencies don’t just happen during office hours. And rather than just leave you with the phone number for a message service we have our most senior staff on call to help you with emergency repairs, whenever they occur.",
     link: false,
     buttonUrl: "/emergency",
-    buttonLabel: "Emergencies",
+    buttonLabel: "Emergencies"
   },
   {
     icon: "contents",
     headline: "Contents insurance",
     text:
       "Many owners and residents aren’t aware that damage to their furniture and possessions is not covered by the buildings insurance. That’s why we help with arranging contents insurance too. And depending on which company the building is insured with, there can be considerable savings by reducing insurances excess amounts.",
-    link: false,
+    link: false
   },
   {
     icon: "bylaw",
     headline: "By-law registration",
     text:
       "With the introduction of the Strata Schemes Management Act 2015, the need for By-laws to be adopted and amended increased greatly. For example, did you know you often need to create a By-law BEFORE you renovate your bathroom?",
-    link: false,
+    link: false
   },
   {
     icon: "contractor",
     headline: "Contractor management",
     text:
       "We all know how complex the licensing, WHS & insurance laws surrounding contractors are these days, so at VJ Ray we enlist the help of an independent company to continually monitor each and every contractor used by all of our strata schemes so that there is never any doubt that they comply.",
-    link: false,
+    link: false
   },
   {
     icon: "voting",
     headline: "Electronic voting",
     text:
       "With the introduction of the Strata Schemes Management Act 2015, there is now the opportunity for strata schemes to elect to make some of the less complex decisions at a meeting by voting electronically, rather than attending the meeting in person.",
-    link: false,
+    link: false
   },
   {
     icon: "service",
     headline: "Online portal",
     text:
       "We understand that its often quicker for you to help yourself, particularly if you are seeking information after hours. So we provide access to much of the commonly needed items through an online portal for each owner.",
-    link: false,
+    link: false
   },
   {
     icon: "sun",
     headline: "Coming soon…",
     text:
       "We are constantly working to improve our help to strata owners and residents by taking advantage of technology as it becomes available. Things such as video meetings, an App to report repairs and maintenance items, and online contractor supervision are just a few of the things we are currently working on, so be sure to check back soon to see how they are coming along.",
-    link: false,
-  },
+    link: false
+  }
 ];
 
 // "Property management" content
@@ -227,14 +223,14 @@ const landlordList = [
     headline: "Prospective tenant database.",
     text:
       "We maintain an extensive database of tenants that are looking for a property right now, so when your property becomes vacant we can offer it to them, rather than wait for them to come looking.",
-    link: false,
+    link: false
   },
   {
     icon: "contents",
     headline: "Regular property inspections.",
     text:
       "More than just finding a new tenant, we need to make sure that they look after your investment property after they move in. So we regularly inspect each property and send you a comprehensive inspection report.",
-    link: false,
+    link: false
   },
   {
     icon: "online",
@@ -242,10 +238,9 @@ const landlordList = [
     text:
       "We recognise that our landlords might not be available when we are open, so you can visit our online landlords portal  to get the latest information about your property.",
     link: false,
-    buttonUrl:
-      "https://www.lookatmyproperty.com.au/934897/Account/Logon?type=1000",
-    buttonLabel: "Login",
-  },
+    buttonUrl: "https://www.lookatmyproperty.com.au/934897/Account/Logon?type=1000",
+    buttonLabel: "Login"
+  }
 ];
 
 const tenantIntro =
@@ -257,14 +252,14 @@ const tenantList = [
     headline: "Complete info and instructions pack.",
     text:
       "When you move in we will give you comprehensive instructions about the property and what is expected of you as the tenant, so its easy for you to move in and live in a hassle free property.",
-    link: false,
+    link: false
   },
   {
     icon: "contractor",
     headline: "Quick and easy repairs.",
     text:
       "No matter how well a property is maintained, we know that little things need repair from time to time and that sometimes emergencies can occur. So just give us a call or lodge your repair request on line and we will immediately contact the landlord for their approval to get the repair underway.",
-    link: false,
+    link: false
   },
   {
     icon: "online",
@@ -272,16 +267,14 @@ const tenantList = [
     text:
       "24 hours a day, 7 days a week you can access your details on line through our tenant portal. to check where your rent is paid to or update your contact details.",
     link: false,
-    buttonUrl:
-      "https://www.lookatmyproperty.com.au/934897/Account/Logon?type=1001",
-    buttonLabel: "Login",
-  },
+    buttonUrl: "https://www.lookatmyproperty.com.au/934897/Account/Logon?type=1001",
+    buttonLabel: "Login"
+  }
 ];
 
 // "Strata" content
 
-const strataIntro =
-  "Intro paragraph selling our Strata Management services going over two or three lines.";
+const strataIntro = "Intro paragraph selling our Strata Management services going over two or three lines.";
 
 const strataList = [
   {
@@ -289,22 +282,22 @@ const strataList = [
     headline: "After-hours emergencies",
     text:
       "We know that emergencies don’t just happen during office hours. And rather than just leave you with the phone number for a message service we have our most senior staff on call to help you with emergency repairs, whenever they occur.",
-    link: false,
+    link: false
   },
   {
     icon: "contractor",
     headline: "Contractor management",
     text:
       "We all know how complex the licensing, WHS & insurance laws surrounding contractors are these days, so at VJ Ray we enlist the help of an independent company to continually monitor each and every contractor used by all of our strata schemes so that there is never any doubt that they comply.",
-    link: false,
+    link: false
   },
   {
     icon: "service",
     headline: "We're open Saturdays",
     text:
       "We know that not everyone has time during the week, so unlike most Stata management companies we also open on Saturdays, so you can pop in, call us or even have your meeting on a Saturday!",
-    link: false,
-  },
+    link: false
+  }
 ];
 
 // "Valuations" content
@@ -314,50 +307,44 @@ const valuationsList = [
     number: "1",
     headline: "Strata Building Insurance Valuations",
     text:
-      "We carry out in excess of 1500 insurance valuations per year and can quickly turn around your request with the full knowledge of the requirements of the Strata Schemes Management Act.",
+      "We carry out in excess of 1500 insurance valuations per year and can quickly turn around your request with the full knowledge of the requirements of the Strata Schemes Management Act."
   },
   {
     number: "2",
     headline: "Stamp Duty Valuations",
     text:
-      "We carry out valuations that meet the requirements of the office of State Revenue to assist you in meeting your stamp duty obligations.",
+      "We carry out valuations that meet the requirements of the office of State Revenue to assist you in meeting your stamp duty obligations."
   },
   {
     number: "3",
     headline: "Capital Gains Tax Valuations",
     text:
-      "Whether current or retrospective reports we have the expertise and knowledge to carry out valuations which will help you when assessing your capital gains tax liability.",
+      "Whether current or retrospective reports we have the expertise and knowledge to carry out valuations which will help you when assessing your capital gains tax liability."
   },
   {
     number: "4",
     headline: "Fair Market Value Valuations",
     text:
-      "Should you need to ascertain the present or retrospective value of a property we can help you by providing a valuation report with the minimum of fuss.",
+      "Should you need to ascertain the present or retrospective value of a property we can help you by providing a valuation report with the minimum of fuss."
   },
   {
     number: "5",
-    headline: "Self Funded Superannuation",
+    headline: "Pre-Sale and Pre-Purchase",
     text:
-      "We are able to supply valuations for superannuation purposes on any property at a set date and on a ongoing basis.",
+      "With over 50 years experience in real estate we have the knowledge and expertise to advise on the pre purchase or the pre-sale values, when you need advice that is independent of the selling agent."
   },
   {
     number: "6",
-    headline: "Pre-Sale and Pre-Purchase",
+    headline: "Family Law Valuations",
     text:
-      "With over 50 years experience in real estate we have the knowledge and expertise to advise on the pre purchase or the pre-sale values, when you need advice that is independent of the selling agent.",
+      "With extensive experience in the Family Law we are in a position to issue valuations for all stages of Family Law proceedings."
   },
   {
     number: "7",
-    headline: "Family Law Valuations",
-    text:
-      "With extensive experience in the Family Law we are in a position to issue valuations for all stages of Family Law proceedings.",
-  },
-  {
-    number: "8",
     headline: "Foreign Ownership Valuations and Advice",
     text:
-      "With the ever changing area of foreign ownership regulation, we are able to provide you with valuation reports to assist you in your dealing with all government departments.",
-  },
+      "With the ever changing area of foreign ownership regulation, we are able to provide you with valuation reports to assist you in your dealing with all government departments."
+  }
 ];
 
 // Facts and forms
@@ -366,42 +353,41 @@ const forms = [
   {
     headline: "Application for Tenancy",
     subheadline: "Download and print to use",
-    link: "/static/forms/Application for Tenancy.pdf",
+    link: "/static/forms/Application for Tenancy.pdf"
   },
   {
     headline: "Ingoing Condition Report",
     subheadline: "A sample PDF",
-    link: "/static/forms/Sample-IngoingConditionReport.pdf",
+    link: "/static/forms/Sample-IngoingConditionReport.pdf"
   },
   {
     headline: "Routine Inspection Report",
     subheadline: "A sample PDF",
-    link: "/static/forms/Sample-RoutineInspectionReport.pdf",
-  },
+    link: "/static/forms/Sample-RoutineInspectionReport.pdf"
+  }
 ];
 
 const factSheets = [
   {
     headline: "New Tenant Checklist",
     subheadline: "",
-    link: "/static/fact-sheets/NSWFT_FS_New_tenant_checklist.pdf",
+    link: "/static/fact-sheets/NSWFT_FS_New_tenant_checklist.pdf"
   },
   {
     headline: "Residential Tenancies Act",
     subheadline: "",
-    link: "/static/fact-sheets/Residential Tenancies Act 2010.pdf",
+    link: "/static/fact-sheets/Residential Tenancies Act 2010.pdf"
   },
   {
     headline: "Residential Tenancies Regulation",
     subheadline: "",
-    link: "/static/fact-sheets/Residential Tenancies Regulation 2010-664.pdf",
+    link: "/static/fact-sheets/Residential Tenancies Regulation 2010-664.pdf"
   },
   {
     headline: "Leading Property Managers of Australia",
-    subheadline:
-      "Why choose an LPMA member to manage your investment property?",
-    link: "/static/fact-sheets/LPMA Brochure.pdf",
-  },
+    subheadline: "Why choose an LPMA member to manage your investment property?",
+    link: "/static/fact-sheets/LPMA Brochure.pdf"
+  }
 ];
 
 export {
@@ -422,5 +408,5 @@ export {
   strataList,
   valuationsList,
   forms,
-  factSheets,
+  factSheets
 };
